56f5480ffda1c3a09db972070497cebe03a0bbf7
Chris Cowan <agentme49@gmail.com>
2017-11-18 06:57:03 -0800

    Use config, allow multiple locations.

--- src/main/java/tech/macil/minecraft/noneuclid/NonEuclidPlugin.java
+++ src/main/java/tech/macil/minecraft/noneuclid/NonEuclidPlugin.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.MemorySection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -16,127 +17,122 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
+import java.util.logging.Level;
 
 public class NonEuclidPlugin extends JavaPlugin implements Listener {
-    public enum Mode {
-        A,
-        B
-    }
-
-    private World mainWorld;
-    private List<Location> SETUP_A;
-    private List<Location> SETUP_B;
-    private Set<Location> BOTH_SETUPS;
-    private Map<Player, Mode> playerModes;
-    private static final Mode DEFAULT_MODE = Mode.A;
-    private static final double CENTER_X = 29;
-    private static final double CENTER_Z = 221;
-    private static final double CENTER_FLOOR = 64;
-    private static final double HALL_HEIGHT = 3;
-    private static final double HALL_WIDTH = 2;
-    private static final double FORGET_DISTANCE_SQUARED = Math.pow(100, 2);
-    private Location CENTER_LOC;
-    private static final Material CONFIG_WALL = Material.STONE;
+    private List<Setup> setups;
+    private Set<Location> allSetupBlockLocations;
 
     @Override
     public void onEnable() {
-        mainWorld = getServer().getWorld("world");
-        CENTER_LOC = new Location(mainWorld, CENTER_X, CENTER_FLOOR, CENTER_Z);
-        playerModes = new HashMap<>();
-        SETUP_A = new ArrayList<>();
-        for (double x : new double[]{CENTER_X - HALL_WIDTH / 2 - 1, CENTER_X + HALL_WIDTH / 2}) {
-            for (double y = CENTER_FLOOR; y < CENTER_FLOOR + HALL_HEIGHT; y++) {
-                for (double z = CENTER_Z - HALL_WIDTH / 2; z < CENTER_Z + HALL_WIDTH / 2; z++) {
-                    SETUP_A.add(new Location(
-                            mainWorld, Location.locToBlock(x),
-                            Location.locToBlock(y), Location.locToBlock(z)
-                    ));
+        saveDefaultConfig();
+
+        setups = new ArrayList<>();
+        allSetupBlockLocations = new HashSet<>();
+        Map<String, Object> configLocations = getConfig().getConfigurationSection("locations").getValues(false);
+        for (Map.Entry<String, Object> entry : configLocations.entrySet()) {
+            Object locationConfig = entry.getValue();
+            if (!(locationConfig instanceof MemorySection)) {
+                getLogger().log(Level.SEVERE, "Error parsing config: locations." + entry.getKey() + " expected to be object");
+                continue;
+            }
+            MemorySection locationSection = (MemorySection) locationConfig;
+            try {
+                if (locationSection.getBoolean("disabled", false)) {
+                    continue;
                 }
+
+                World world = getServer().getWorld(locationSection.getString("world"));
+                long x = locationSection.getLong("x");
+                long y = locationSection.getLong("y");
+                long z = locationSection.getLong("z");
+                int width = locationSection.getInt("wall_width", 2);
+                int height = locationSection.getInt("wall_height", 3);
+                Material material = Material.valueOf(locationSection.getString("material", "STONE"));
+                Setup.Path defaultPath = Setup.Path.valueOf(locationSection.getString("default_path", "NorthSouth"));
+
+                Location loc = new Location(world, x, y, z);
+                Setup setup = new Setup(loc, width, height, material, defaultPath);
+                setups.add(setup);
+                allSetupBlockLocations.addAll(setup.getNorthSouthLocations());
+                allSetupBlockLocations.addAll(setup.getEastWestLocations());
+            } catch (Exception e) {
+                getLogger().log(Level.SEVERE, "Error parsing config locations." + entry.getKey(), e);
             }
         }
-        SETUP_B = new ArrayList<>();
-        for (double z : new double[]{CENTER_Z - HALL_WIDTH / 2 - 1, CENTER_Z + HALL_WIDTH / 2}) {
-            for (double y = CENTER_FLOOR; y < CENTER_FLOOR + HALL_HEIGHT; y++) {
-                for (double x = CENTER_X - HALL_WIDTH / 2; x < CENTER_X + HALL_WIDTH / 2; x++) {
-                    SETUP_B.add(new Location(
-                            mainWorld, Location.locToBlock(x),
-                            Location.locToBlock(y), Location.locToBlock(z)
-                    ));
-                }
-            }
-        }
-        BOTH_SETUPS = new HashSet<>();
-        BOTH_SETUPS.addAll(SETUP_A);
-        BOTH_SETUPS.addAll(SETUP_B);
         for (Player player : getServer().getOnlinePlayers()) {
-            setupPlayer(player);
+            renderForPlayer(player);
         }
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        List<Block> blocks = new ArrayList<>(BOTH_SETUPS.size());
-        for (Location loc : BOTH_SETUPS) {
+        List<Block> blocks = new ArrayList<>(allSetupBlockLocations.size());
+        for (Location loc : allSetupBlockLocations) {
             blocks.add(loc.getBlock());
         }
-        Location loc = new Location(null, 0, 0, 0);
+        final Location tLoc = new Location(null, 0, 0, 0);
         for (Player player : getServer().getOnlinePlayers()) {
-            if (player.getWorld() != mainWorld) {
+            for (Block block : blocks) {
+                block.getLocation(tLoc);
+                if (player.getWorld() != tLoc.getWorld()) {
+                    continue;
+                }
+                player.sendBlockChange(tLoc, block.getType(), block.getData());
+            }
+        }
+        setups = null;
+        allSetupBlockLocations = null;
+    }
+
+    private void renderForPlayer(Player player) {
+        renderForPlayer(player, false);
+    }
+
+    private void renderForPlayer(Player player, boolean forceRender) {
+        Location playerLoc = player.getLocation();
+        for (Setup setup : setups) {
+            Map<Player, Setup.Path> currentPlayerPaths = setup.getCurrentPlayerPaths();
+            if (!setup.isLocationClose(playerLoc)) {
+                currentPlayerPaths.remove(player);
                 continue;
             }
-            for (Block block : blocks) {
-                block.getLocation(loc);
-                player.sendBlockChange(loc, block.getType(), block.getData());
-            }
-        }
-        playerModes = null;
-    }
-
-    private void setupPlayer(Player player) {
-        setupPlayer(player, false);
-    }
-
-    private void setupPlayer(Player player, boolean forceRender) {
-        Location playerLoc = player.getLocation();
-        if (!locationIsClose(playerLoc)) {
-            playerModes.remove(player);
-            return;
-        }
-        Mode currentMode = playerModes.get(player);
-        Mode newMode = getModeForLocation(playerLoc);
-        if (newMode == null) {
-            if (currentMode == null) {
-                newMode = DEFAULT_MODE;
-            } else {
-                if (!forceRender) {
-                    return;
+            Setup.Path currentPath = currentPlayerPaths.get(player);
+            Setup.Path newPath = setup.getPathForLocation(playerLoc);
+            if (newPath == null) {
+                if (currentPath == null) {
+                    newPath = setup.getDefaultPath();
                 } else {
-                    newMode = currentMode;
+                    if (!forceRender) {
+                        continue;
+                    } else {
+                        newPath = currentPath;
+                    }
                 }
+            } else if (currentPath == newPath && !forceRender) {
+                continue;
             }
-        } else if (currentMode == newMode && !forceRender) {
-            return;
-        }
 
-        List<Block> realBlocks = new ArrayList<>();
-        for (Location loc : newMode == Mode.B ? SETUP_A : SETUP_B) {
-            realBlocks.add(loc.getBlock());
+            List<Block> realBlocks = new ArrayList<>();
+            for (Location loc : newPath == Setup.Path.EastWest ? setup.getNorthSouthLocations() : setup.getEastWestLocations()) {
+                realBlocks.add(loc.getBlock());
+            }
+            Location tLoc = new Location(null, 0, 0, 0);
+            for (Block block : realBlocks) {
+                block.getLocation(tLoc);
+                player.sendBlockChange(tLoc, block.getType(), block.getData());
+            }
+            for (Location loc : newPath == Setup.Path.NorthSouth ? setup.getNorthSouthLocations() : setup.getEastWestLocations()) {
+                player.sendBlockChange(loc, setup.getMaterial(), (byte) 0);
+            }
+            currentPlayerPaths.put(player, newPath);
         }
-        Location mloc = new Location(null, 0, 0, 0);
-        for (Block block : realBlocks) {
-            block.getLocation(mloc);
-            player.sendBlockChange(mloc, block.getType(), block.getData());
-        }
-        for (Location loc : newMode == Mode.A ? SETUP_A : SETUP_B) {
-            player.sendBlockChange(loc, CONFIG_WALL, (byte) 0);
-        }
-        playerModes.put(player, newMode);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerMove(PlayerMoveEvent event) {
-        setupPlayer(event.getPlayer());
+        renderForPlayer(event.getPlayer());
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
@@ -146,49 +142,29 @@ public class NonEuclidPlugin extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
             if (player.isOnline()) {
-                setupPlayer(player, true);
+                renderForPlayer(player, true);
             }
         });
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerQuit(PlayerQuitEvent event) {
-        playerModes.remove(event.getPlayer());
-    }
-
-    private boolean locationIsClose(Location loc) {
-        return loc.getWorld() == mainWorld &&
-                CENTER_LOC.distanceSquared(loc) < FORGET_DISTANCE_SQUARED;
-    }
-
-    private Mode getModeForLocation(Location loc) {
-        assert loc.getWorld() == mainWorld;
-        if (
-                Math.abs(loc.getX() - CENTER_X) < HALL_WIDTH &&
-                        Math.abs(loc.getZ() - CENTER_Z) < HALL_WIDTH
-                ) {
-            return null;
+        for (Setup setup : setups) {
+            setup.getCurrentPlayerPaths().remove(event.getPlayer());
         }
-        boolean a1 = loc.getZ() > loc.getX() + CENTER_Z - CENTER_X;
-        boolean a2 = loc.getZ() > -loc.getX() + CENTER_Z + CENTER_X;
-        return a1 == a2 ? Mode.A : Mode.B;
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerInteract(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
         if (event.getAction() != Action.LEFT_CLICK_BLOCK) {
             return;
         }
-        Location playerLoc = player.getLocation();
-        if (!locationIsClose(playerLoc)) {
-            return;
-        }
         Location clickedLoc = event.getClickedBlock().getLocation();
-        if (BOTH_SETUPS.contains(clickedLoc)) {
+        if (allSetupBlockLocations.contains(clickedLoc)) {
+            Player player = event.getPlayer();
             getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
                 if (player.isOnline()) {
-                    setupPlayer(player, true);
+                    renderForPlayer(player, true);
                 }
             });
         }
---
+++ src/main/java/tech/macil/minecraft/noneuclid/Setup.java
@@ -0,0 +1,99 @@
+package tech.macil.minecraft.noneuclid;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Setup {
+    private static final double FORGET_DISTANCE_SQUARED = Math.pow(160, 2);
+
+    public enum Path {
+        NorthSouth,
+        EastWest
+    }
+
+    private final Location center;
+    private final int width;
+    private final Material material;
+    private final Path defaultPath;
+    private final List<Location> northSouthLocations;
+    private final List<Location> eastWestLocations;
+    private final Map<Player, Path> currentPlayerPaths = new HashMap<>();
+
+    public Setup(Location center, int width, int height, Material material, Path defaultPath) {
+        this.center = center;
+        this.width = width;
+        this.material = material;
+        this.defaultPath = defaultPath;
+
+        northSouthLocations = new ArrayList<>();
+        for (double x : new double[]{center.getX() - (double) width / 2 - 1, center.getX() + (double) width / 2}) {
+            for (double y = center.getY(); y < center.getY() + height; y++) {
+                for (double z = center.getZ() - (double) width / 2; z < center.getZ() + (double) width / 2; z++) {
+                    northSouthLocations.add(new Location(
+                            center.getWorld(), Location.locToBlock(x),
+                            Location.locToBlock(y), Location.locToBlock(z)
+                    ));
+                }
+            }
+        }
+        eastWestLocations = new ArrayList<>();
+        for (double z : new double[]{center.getZ() - (double) width / 2 - 1, center.getZ() + (double) width / 2}) {
+            for (double y = center.getY(); y < center.getY() + height; y++) {
+                for (double x = center.getX() - (double) width / 2; x < center.getX() + (double) width / 2; x++) {
+                    eastWestLocations.add(new Location(
+                            center.getWorld(), Location.locToBlock(x),
+                            Location.locToBlock(y), Location.locToBlock(z)
+                    ));
+                }
+            }
+        }
+    }
+
+    public Location getCenter() {
+        return center;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public Path getDefaultPath() {
+        return defaultPath;
+    }
+
+    public List<Location> getNorthSouthLocations() {
+        return northSouthLocations;
+    }
+
+    public List<Location> getEastWestLocations() {
+        return eastWestLocations;
+    }
+
+    public Map<Player, Path> getCurrentPlayerPaths() {
+        return currentPlayerPaths;
+    }
+
+    public boolean isLocationClose(Location loc) {
+        return loc.getWorld() == center.getWorld() &&
+                center.distanceSquared(loc) < FORGET_DISTANCE_SQUARED;
+    }
+
+    public Path getPathForLocation(Location loc) {
+        assert loc.getWorld() == center.getWorld();
+        if (
+                Math.abs(loc.getX() - center.getX()) <= width &&
+                        Math.abs(loc.getZ() - center.getZ()) <= width
+                ) {
+            return null;
+        }
+        boolean a1 = loc.getZ() > loc.getX() + center.getZ() - center.getX();
+        boolean a2 = loc.getZ() > -loc.getX() + center.getZ() + center.getX();
+        return a1 == a2 ? Path.NorthSouth : Path.EastWest;
+    }
+}
