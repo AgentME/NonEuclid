fb09e932d5f92d35355ee2bd97f57a48271f7b94
Chris Cowan <agentme49@gmail.com>
2017-11-21 23:42:41 -0800

    Remove extra import.

--- src/main/java/tech/macil/minecraft/noneuclid/NonEuclidPlugin.java
+++ src/main/java/tech/macil/minecraft/noneuclid/NonEuclidPlugin.java
@@ -22,7 +22,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
