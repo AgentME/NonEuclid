7acfe002a372ba3c4ee628f152e9b74f44537c91
Chris Cowan <agentme49@gmail.com>
2017-11-23 02:00:17 -0800

    Record the number of locations.
    Fix metrics not being enabled if zero locations are configured.

--- src/main/java/tech/macil/minecraft/noneuclid/NonEuclidPlugin.java
+++ src/main/java/tech/macil/minecraft/noneuclid/NonEuclidPlugin.java
@@ -95,6 +95,25 @@ public class NonEuclidPlugin extends JavaPlugin implements Listener {
             }
         }
 
+        try {
+            Metrics metrics = new Metrics(this);
+
+            // Copy the size into a variable here instead of having the metrics
+            // thread access the intersections list.
+            final int locationsCount = intersections.size();
+            Metrics.Graph locationsCountGraph = metrics.createGraph("Locations");
+            locationsCountGraph.addPlotter(new Metrics.Plotter() {
+                @Override
+                public int getValue() {
+                    return locationsCount;
+                }
+            });
+
+            metrics.start();
+        } catch (IOException e) {
+            // Failed to submit the stats :-(
+        }
+
         if (intersections.size() == 0) {
             // Don't register event listeners if nothing is configured to be used.
             return;
@@ -179,13 +198,6 @@ public class NonEuclidPlugin extends JavaPlugin implements Listener {
                 }
             }
         });
-
-        try {
-            Metrics metrics = new Metrics(this);
-            metrics.start();
-        } catch (IOException e) {
-            // Failed to submit the stats :-(
-        }
     }
 
     @Override
